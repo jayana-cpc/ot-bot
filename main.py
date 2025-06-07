8dc3c8cb00190d529c344008f966a78bf84c7856
Jayana Nanayakkara <jchdnjc@gmail.com>
2025-06-07 13:58:48 -0700

    options sides

--- main.py
+++ main.py
@@ -1,17 +1,22 @@
 import os
 from dotenv import load_dotenv
 from api import TTAPI
+from order import TTOption, TTOptionSide, TTOrder
+import asyncio
+from tastytrade import Session, DXLinkStreamer
+from tastytrade.dxfeed import Greeks
+
 load_dotenv()
 
 # Constants
 CERT_URL: str = "https://api.cert.tastyworks.com"
 PROD_URL: str = "https://api.tastyworks.com"
 CERT_WSS: str = "wss://streamer.tastyworks.com"
-LOGIN: str = os.getenv("LOGIN")
-PASSWORD: str = os.getenv("PASSWORD")
-TOKEN: str = os.getenv("TOKEN")
-EXP: float = float(os.getenv("EXP") or 0)
+LOGIN: str = str(os.getenv("LOGIN"))
+PASSWORD: str = "kyfnuq-6dahqo-bIhzim" # str(os.getenv("PASSWORD"))
 
+TOKEN: str = str(os.getenv("TOKEN"))
+EXP: float = float(os.getenv("EXP") or 0)
 
 tastytrade = TTAPI(PROD_URL, LOGIN, PASSWORD, TOKEN, EXP)
 
@@ -21,14 +26,27 @@ if not tastytrade.login():
 if not tastytrade.validate():
     exit()
 
-symbol: str = input("Ticker: ")
+root_symbol: str = input("Ticker: ")
 
-chains = tastytrade.get_chains(symbol)
+chains = tastytrade.get_chains(root_symbol)
 
-tastytrade.see_chains(chains, 10)
+option = chains[0]
+symbol = option['symbol']
+date = option['expiration-date']
+side = TTOptionSide.CALL if option['option-type'] == 'C' else TTOptionSide.PUT
+strike = float(option['strike-price'])
 
-options = tastytrade.sort_chain(chains, 0, 0, 100000, "C")
+# option = TTOption(symbol, date, side, strike)
+print(symbol)
 
-tastytrade.see_chains(options, len(options))
+order = TTOrder
+
+
+
+# tastytrade.see_chains(chains, 10)
+
+# options = tastytrade.sort_chain(chains, 0, 0, 100000, "C")
+
+# tastytrade.see_chains(options, len(options))
 
 
--- order.py
+++ order.py
@@ -1,46 +1,92 @@
 import json
 from enum import Enum
+from typing import Any
 
 class TTOrderType(Enum):
-  LIMIT = 'Limit'
-  MARKET = 'Market'
+	LIMIT = 'Limit'
+	MARKET = 'Market'
+	STOP = 'Stop'
 
 class TTPriceEffect(Enum):
-  CREDIT = 'Credit'
-  DEBIT = 'Debit'
+	CREDIT = 'Credit'
+	DEBIT = 'Debit'
 
 class TTOrderStats(Enum):
-  RECEIVED = 'Received'
-  CANCELLED = 'Cancelled'
-  FILLED = 'Filled'
-  EXPIRED = 'Expired'
-  LIVE = 'Live'
-  REJECTED = 'Rejected'
+	RECEIVED = 'Received'
+	CANCELLED = 'Cancelled'
+	FILLED = 'Filled'
+	EXPIRED = 'Expired'
+	LIVE = 'Live'
+	REJECTED = 'Rejected'
 
 class TTTimeInForce(Enum):
-  DAY = 'Day'
-  GTC = 'GTC'
-  GTD = 'GTD'
+	DAY = 'Day'
+	GTC = 'GTC'
+	GTD = 'GTD'
 
 class TTInstrumentType(Enum):
-  EQUITY = 'Equity'
-  EQUITY_OPTION = 'Equity Option'
-  FUTURE = 'Future'
-  FUTURE_OPTION = 'Future Option'
-  NOTIONAL_MARKET = 'Notional Market'
+	EQUITY = 'Equity'
+	EQUITY_OPTION = 'Equity Option'
+	FUTURE = 'Future'
+	FUTURE_OPTION = 'Future Option'
+	NOTIONAL_MARKET = 'Notional Market'
 
 class TTLegAction(Enum):
-  STO = 'Sell to Open'
-  STC = 'Sell to Close'
-  BTO = 'Buy to Open'
-  BTC = 'Buy to Close'
+	STO = 'Sell to Open'
+	STC = 'Sell to Close'
+	BTO = 'Buy to Open'
+	BTC = 'Buy to Close'
 
 class TTOptionSide(Enum):
-  PUT = 'P'
-  CALL = 'C'
+	PUT = 'P'
+	CALL = 'C'
 
 class TTOption:
-  ...
+
+	def __init__(self, symbol: str, exp: str, side: TTOptionSide, strike: float) -> None:
+		symbol = symbol + (' ' * (6 - len(symbol)))
+		strike = float(('0'* (8 - len(str(strike * 1000)))) + str(strike * 1000))
+		exp = exp.split('-')[0][2:] + exp.split('-')[1] + exp.split('-')[2]
+		self.symbol = symbol + exp + side.value + str(strike)
+
+
+    
 
 class TTOrder:
-   ... 
+   
+	def __init__(self, tif: TTTimeInForce, price: float, price_effect: TTPriceEffect, order_type: TTOrderType) -> None:
+		self.tif = tif
+		self.order_type = order_type
+		self.price = '{:2f}'.format(price)
+		self.price_effect = price_effect
+		self.legs = []
+	
+	def add_leg(self, instrument_type: TTInstrumentType, symbol: str, quantity: int, action: TTLegAction):
+		if len(self.legs) >= 4:
+			print('Error: cannot have more than 4 legs per order.')
+			return 
+	
+		if instrument_type is None or symbol == None or quantity == 0 or action is None:
+			print(f'Invalid parameters')
+			print(f'instrument_type: {instrument_type}')
+			print(f'symbol: {symbol}')
+			print(f'quantity')
+		
+		self.legs.append({
+			'instrument-type': instrument_type.value,
+			'symbol': symbol,
+			'quantity': quantity,
+			'action': action.value
+			})
+	def build_order(self) -> dict:
+		self.body = {
+		'time-in-force': self.tif.value,
+		'price': self.price,
+		'price-effect': self.price_effect.value,
+		'order-type': self.order_type.value,
+		'legs': self.legs
+		}
+		print(json.dumps(self.body))
+		return self.body
+
+
